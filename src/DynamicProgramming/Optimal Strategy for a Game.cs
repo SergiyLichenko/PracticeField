b15c82cd9f9f4fb86fff494f578cabcfc0081d6c
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-03-06 13:57:59 +0200

    Update and rename Optimal Strategy For a Game.cs to Optimal Strategy for a Game.cs

--- src/DynamicProgramming/Optimal Strategy for a Game.cs
+++ src/DynamicProgramming/Optimal Strategy for a Game.cs
@@ -1,4 +1,7 @@
-﻿using System;
+/* Problem Statement
+http://www.geeksforgeeks.org/dynamic-programming-set-31-optimal-strategy-for-a-game/ */
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
