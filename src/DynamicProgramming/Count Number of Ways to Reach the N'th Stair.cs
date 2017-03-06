deafad566bcc9d559bde35fb535b068c5a9ba9b4
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-03-06 09:32:25 +0200

    Count Number of Ways to Reach the N'th Stair

---
+++ src/DynamicProgramming/Count Number of Ways to Reach the N'th Stair.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+
+namespace GitHub
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int n = 9;
+            int count = GetCountStair(n);
+
+            Console.WriteLine($"There is {count} of ways to reach {n}th stair");
+            Console.ReadLine();
+        }
+
+        #region Number of ways n stair
+
+        private static int GetCountStair(int height)
+        {
+            int first = 1;
+            int second = 1;
+
+            for (int i = 1; i < height; i++)
+            {
+                second += first;
+                first = second - first;
+            }
+            return second;
+        }
+
+        #endregion
+    }
+
+}
