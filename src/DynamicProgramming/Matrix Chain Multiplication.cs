f2e95f32f9336644303875c39062bde3a060dfaa
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-03-06 13:25:21 +0200

    Update Matrix Chain Multiplication.cs

--- src/DynamicProgramming/Matrix Chain Multiplication.cs
+++ src/DynamicProgramming/Matrix Chain Multiplication.cs
@@ -1,4 +1,7 @@
-﻿using System;
+/* Problem Statement
+http://www.geeksforgeeks.org/dynamic-programming-set-8-matrix-chain-multiplication/ */
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
