a3ff8c09acf0ef05aa49da861e9d4ae613892f6f
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-03-06 20:19:16 +0200

    Update Palindrome Partitioning.cs

--- src/DynamicProgramming/Palindrome Partitioning.cs
+++ src/DynamicProgramming/Palindrome Partitioning.cs
@@ -1,4 +1,7 @@
-﻿using System;
+/* Problem Statement
+http://www.geeksforgeeks.org/dynamic-programming-set-17-palindrome-partitioning/ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
