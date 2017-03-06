2f564c6fdf1c2a7372a2b3875101f98b0e4d67c3
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-03-06 20:51:49 +0200

    Min Number of Jumps

---
+++ src/DynamicProgramming/Min Number of Jumps.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] jumps = { 2,3,1,1,4};
+
+            int minNumber = MinimumNumberOfJumps(jumps);
+
+            if (minNumber == Int32.MaxValue)
+                Console.WriteLine("You cannot reach the end");
+            else
+                Console.WriteLine($"The minimum number of " +
+                                  $"jumps to reach the end is {minNumber}");
+            Console.ReadLine();
+        }
+
+        #region Minimum number of jumps
+
+        private static int MinimumNumberOfJumps(int[] input)
+        {
+            var data = new int[input.Length];
+            for (int i = 1; i < data.Length; i++)
+                data[i] = Int32.MaxValue;
+
+            for (int i = 1; i < data.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (input[j] >= i - j && data[j] + 1 < data[i])
+                        data[i] = data[j] + 1;
+                }
+            }
+
+            return data.Last();
+        }
+
+        #endregion
+    }
+
+}
