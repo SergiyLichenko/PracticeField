a1b39951e350dec4a1b3afe81c1e64019857682b
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-03-06 13:21:28 +0200

    Update Longest Palindromic Subsequence.cs

--- src/DynamicProgramming/Longest Palindromic Subsequence.cs
+++ src/DynamicProgramming/Longest Palindromic Subsequence.cs
@@ -1,4 +1,7 @@
-﻿using System;
+/* Problem Statement
+http://www.geeksforgeeks.org/dynamic-programming-set-12-longest-palindromic-subsequence/ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
