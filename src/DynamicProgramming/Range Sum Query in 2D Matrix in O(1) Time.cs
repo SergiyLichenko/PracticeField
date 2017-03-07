5274f94b12300e09928634c68c1e0ed9789ccd8f
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-03-07 09:02:27 +0200

    Update Range Sum Query in 2D Matrix in O(1) Time.cs

--- src/DynamicProgramming/Range Sum Query in 2D Matrix in O(1) Time.cs
+++ src/DynamicProgramming/Range Sum Query in 2D Matrix in O(1) Time.cs
@@ -1,4 +1,7 @@
-﻿using System;
+/* Problem Statement
+https://leetcode.com/problems/range-sum-query-2d-immutable/?tab=Description */
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -24,13 +27,10 @@ namespace GitHub
 
             Console.WriteLine($"Sum of submatrix: {sum1}");
             Console.ReadLine();
-        }
+        }       
 
-        private static int QuerySum(int[,] sumMatrix, int row1, int col1, int row2, int col2)
-            => sumMatrix[row2 + 1, col2 + 1] - sumMatrix[row2 + 1, col1] -
-                    sumMatrix[row1, col2 + 1] + sumMatrix[row1, col1];
-
-        #region Sum sub matrix in 2D array
+        #region Range Sum Query in 2D Matrix in O(1) Time
+            
         private static int[,] GetSumMatrix(int[,] input)
         {
             int[,] sumMatrix = new int[input.GetLength(0) + 1, input.GetLength(1) + 1];
@@ -47,7 +47,10 @@ namespace GitHub
             return sumMatrix;
         }
 
+        private static int QuerySum(int[,] sumMatrix, int row1, int col1, int row2, int col2)
+            => sumMatrix[row2 + 1, col2 + 1] - sumMatrix[row2 + 1, col1] -
+                    sumMatrix[row1, col2 + 1] + sumMatrix[row1, col1];
+        
         #endregion
     }
-
 }
