08c714a557820f8eef2aa5614995cdce314966a5
Sergiy Lichenko <sergiy.lichenko@outlook.com>
2017-03-07 07:55:45 +0200

    Max Sum Increasing Subsequence

---
+++ src/DynamicProgramming/Max Sum Increasing Subsequence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = new int[] {1,101,2,3,100,4,5 };
+            int maxSum = MaxSumIncreasingSubsequence(numbers);
+
+            Console.WriteLine($"The maximum sum " +
+                              $"of increasing subsequence is {maxSum}");
+
+            Console.ReadLine();
+        }
+
+        #region Maximum Sub Increasing Subsequence
+
+        private static int MaxSumIncreasingSubsequence(int[] numbers)
+        {
+            var data = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+                data[i] = numbers[i];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                        data[i] = Math.Max(data[i], data[j] + numbers[i]);
+                }
+            }
+
+            return data.Max();
+        }
+
+        #endregion
+    }
+
+}
